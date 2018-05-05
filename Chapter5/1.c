294e2d602f3c2e8e4c114f4aaae897988acb44bf
ShijiaYuan <38323700+shijyuen@users.noreply.github.com>
2018-05-05 19:55:41 +0800

    Update 1.c

--- Chapter5/1.c
+++ Chapter5/1.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #define mins_per_hour 60
 int main(void){
-	int min = 1;
+	int min = 1;//初始化一个变量，大于0就行
 	
 	while(min > 0){
 		printf("enter a time:\n");
 		scanf("%d", &min);
 		printf("%d mins is %d hour %d min\n", min, min / mins_per_hour, min % mins_per_hour);
+		//两种除法自己好好体会
 	}
 	
 	return 0;
