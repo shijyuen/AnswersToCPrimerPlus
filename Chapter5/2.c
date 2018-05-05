2482f919a2096abf96251cb12f072f34d4db9a7b
ShijiaYuan <38323700+shijyuen@users.noreply.github.com>
2018-05-05 20:00:01 +0800

    Update 2.c

--- Chapter5/2.c
+++ Chapter5/2.c
@@ -5,11 +5,12 @@ int main(void){
 	printf("enter a num:\n");
 	scanf("%d", &num);
 	
-	int end = num + 10;
+	int end = num + 10;//声明结束的数字
 	
 	while(num < end){
 		num++;
 		printf("%d\n", num);
+		//注意顺序！ 影响边界结果。而且这个不是唯一答案
 	}
 	return 0; 
 }
