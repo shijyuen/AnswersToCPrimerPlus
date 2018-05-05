cc41a59e7880cb121d7cd81f3a1f62a816231308
ShijiaYuan <38323700+shijyuen@users.noreply.github.com>
2018-05-05 20:09:00 +0800

    Update 3.c

--- Chapter5/3.c
+++ Chapter5/3.c
@@ -10,3 +10,18 @@ int main(void){
 	}
 	return 0;
 }
+
+//下面这个改了下条件， 输入负数时不会继续打印直接退出循环
+#include <stdio.h>
+int main(void){
+	int days = 1;
+	const int days_per_week = 7; 
+	
+	printf("enter days:\n");
+	while(scanf("%d", &days) == 1, days > 0)//了解下scanf的返回值，逗号运算符
+	{
+		printf("%d days are %d weeks, %d days.\n", days, days / days_per_week, days % days_per_week);
+		printf("enter days:\n");
+	}
+	return 0;
+}
